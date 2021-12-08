025f10998bf59f061440c4002dd2eed04437b98d
Inna Belopolskaia <innabelopolskaya@gmail.com>
2021-12-08 16:12:11 +0300

    Prepared all Java classes for the project

---
+++ src/GamePanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
