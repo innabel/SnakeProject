bef6aa00322347bff6ecd7a807761cbd7a02e474
Inna Belopolskaia <innabelopolskaya@gmail.com>
2021-12-08 18:41:36 +0300

    Work on GamePanel class: - changed the Unit size from 25 to 20; == larger grid, more units to fit in; - added RGB colors - adjusted background, grid lines and apples colors; - finished the "move" method; - added the body of the snake (in 'draw' method); - made the snake move - updated the 'actionPerformed' method; - updated the 'checkCollisions' method to make sure that the snake cannot go out of the boundaries;

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     // how big we want object to appear in our game:
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 20;
     // how many object we can actually fit in this game:
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     // the higher the number, the slower the game is
@@ -27,12 +27,13 @@ public class GamePanel extends JPanel implements ActionListener {
     Timer timer;
     Random random;
 
+    // our constructor
     GamePanel() {
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         // TODO later we can offer different options of the whole new game design with different
         //  colors
-        this.setBackground(Color.white);
+        this.setBackground(new Color(152,214,220));
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
@@ -52,22 +53,62 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
-        g.setColor(Color.lightGray);
+        // drawing the grid on the field
+        g.setColor(new Color(62, 189, 219));
         for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
             // drawing vertical lines
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
             // drawing horizontal lines
             g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
 
+        // drawing the apple
+        g.setColor(new Color(232, 36, 39));
+        // TODO can change that later with object in the shape of an apple;
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        
+        // drawing the snake
+        for (int i = 0; i < bodyParts; i++) {
+            // case when we are dealing with the head of the snake
+            if(i == 0) {
+                g.setColor(new Color(44, 100, 32));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(123, 169, 96));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            
         }
     }
 
     public void newApple() {
+        // generating the coordinates of the new apple;
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE)) * UNIT_SIZE;
 
     }
 
+    // moving the snake across our field
     public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
 
+        switch(direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
     }
 
     public void checkApple() {
@@ -75,6 +116,33 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkCollisions() {
+        // checking if head collides with body
+        for (int i = bodyParts; i > 0; i--) {
+            if((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false; // triggers the gameOver method
+            }
+        }
+        // checking if head touches left border
+        if(x[0] < 0) {
+            running = false;
+        }
+        // checking if head touches right border
+        if(x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        // checking if head touches top border
+        if(y[0] < 0) {
+            running = false;
+        }
+        // checking if head touches bottom border
+        if(y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
+        // stopping the timer
+        if(!running) {
+            timer.stop();
+        }
 
     }
 
@@ -84,7 +152,12 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter {
